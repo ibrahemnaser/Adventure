cb6bb4403dd8f784cd7c1b41138c7937c33b456e
ibrahemnaser <i.naser4040@gmail.com>
2022-08-28 22:40:24 +0200

    Update arrow up transition

--- js/methods/arrowUp.js
+++ js/methods/arrowUp.js
@@ -1,6 +1,14 @@
 const arrowUp = document.querySelector(".arrow-up");
 
 export function getPageUp() {
+  window.addEventListener("scroll", () => {
+    if (scrollY >= 500) {
+      arrowUp.classList.add("show");
+    }
+    if (scrollY == 0) {
+      arrowUp.classList.remove("show");
+    }
+  });
   arrowUp.addEventListener("click", () => {
     window.scrollTo(0, 0);
   });
