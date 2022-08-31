4d94d0d9bf766bccbf7915b825450955a941fb83
ibrahemnaser <82912094+ibrahemnaser@users.noreply.github.com>
2022-08-31 17:38:08 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 // import functions
-import { isToggled } from "./Adventure/js/methods/toggler.js";
-import { isActive } from "./Adventure/js/methods/active.js";
+import { isToggled } from "Adventure/js/methods/toggler.js";
+import { isActive } from "Adventure/js/methods/active.js";
 import {
   switchColor,
   setPrimaryColor,
-} from "./Adventure/js/methods/colorSwitcher.js";
-import { changeMode } from "./Adventure/js/methods/themeMode.js";
-import { toggleCarousel } from "./Adventure/js/methods/shareCarousel.js";
-import { setOpacityPlans } from "./Adventure/js/methods/plans.js";
-import { getPageUp } from "./Adventure/js/methods/arrowUp.js";
-import { handleLoading } from "./Adventure/js/methods/loading.js";
+} from "Adventure/js/methods/colorSwitcher.js";
+import { changeMode } from "Adventure/js/methods/themeMode.js";
+import { toggleCarousel } from "Adventure/js/methods/shareCarousel.js";
+import { setOpacityPlans } from "Adventure/js/methods/plans.js";
+import { getPageUp } from "Adventure/js/methods/arrowUp.js";
+import { handleLoading } from "Adventure/js/methods/loading.js";
 
 // implement fuctions
 isToggled();
